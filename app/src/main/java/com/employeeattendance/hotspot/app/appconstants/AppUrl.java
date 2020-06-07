027f1b45755407b650a082475b4bf4f043770b1c
Rohit <softeng.rohit@gmail.com>
2020-06-08 00:02:06 +0530

    EmployeeAttendance

---
+++ app/src/main/java/com/employeeattendance/hotspot/app/appconstants/AppUrl.java
@@ -0,0 +1,7 @@
+package com.employeeattendance.hotspot.app.appconstants;
+
+public class AppUrl {
+
+    public static String BaseUrl = "http://hotspotgroup.in/hot/api/authentication/";
+    public static String VerifyPhone = BaseUrl+"verify-phone";
+}
---
+++ app/src/main/java/com/employeeattendance/hotspot/app/remote/APIInterface.java
@@ -0,0 +1,9 @@
+package com.employeeattendance.hotspot.app.remote;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface APIInterface {
+//    @GET("verify-phone")
+//    Call<Multi> doCheckMobileStatus();
+}
