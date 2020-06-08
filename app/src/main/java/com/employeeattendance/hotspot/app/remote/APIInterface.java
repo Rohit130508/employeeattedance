45e4919f1cbf87c5578bab607c7de1610ff392a7
Rohit <softeng.rohit@gmail.com>
2020-06-09 00:09:03 +0530

    EmployeeAttendance

--- app/src/main/java/com/employeeattendance/hotspot/app/remote/APIInterface.java
+++ app/src/main/java/com/employeeattendance/hotspot/app/remote/APIInterface.java
@@ -10,19 +10,26 @@ import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
 
 public interface APIInterface {
+
+
+
+
     @POST("verify-phone")
     Call<VerifyPhoneModal> doCheckMobileStatus(@Header("Content-Type") String con_type, @Body VerifyPhoneRequest request);
 
 //    @POST("verify-phone")
 //    Call<VerifyPhoneModal> doVerifyPhoneStatus(@Header("Content-Type") String con_type, @Field("phone") String phone);
 
+    @Headers({
+            "Accept: application/json",
+            "X-API-KEY: 25d55ad283aa400af464c76d713c07ad",
+            "Authorization: Basic YWRtaW46YWRtaW5AaG90c3BvdA=="
+    })
     @POST("verify-phone")
-    Call<VerifyPhoneModal> doVerifyPhoneStatus(@Header("Content-Type")String contype,
-                                               @Header("x-api-key")String header,
-                                               @Header("Authorization")String author,
-                                               @Body RequestBody body);
+    Call<VerifyPhoneModal> doVerifyPhoneStatus( @Field("phone") String phone);
 }
--- app/src/main/java/com/employeeattendance/hotspot/app/ui/auth/LoginActivity.java
+++ app/src/main/java/com/employeeattendance/hotspot/app/ui/auth/LoginActivity.java
@@ -3,18 +3,13 @@ package com.employeeattendance.hotspot.app.ui.auth;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.employeeattendance.hotspot.app.R;
-import com.employeeattendance.hotspot.app.modal.auth.VerifyPhoneModal;
 import com.employeeattendance.hotspot.app.remote.APIInterface;
 import com.employeeattendance.hotspot.app.remote.CustomNetwork;
-import com.google.gson.Gson;
-
-import androidx.appcompat.app.AppCompatActivity;
-import okhttp3.MultipartBody;
-import okhttp3.RequestBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -35,30 +30,6 @@ public class LoginActivity extends AppCompatActivity {
 
     void checkPhoneStatus()
     {
-
-        RequestBody requestBody = new MultipartBody.Builder()
-                .setType(MultipartBody.FORM)
-                .addFormDataPart("phone", "9453284838")
-                .build();
-        final Call<VerifyPhoneModal> listCall = mAPIService.doVerifyPhoneStatus("multipart/form-data","25d55ad283aa400af464c76d713c07ad",
-                "Basic YWRtaW46YWRtaW5AaG90c3BvdA==",
-                requestBody);
-
-        listCall.enqueue(new Callback<VerifyPhoneModal>() {
-            @Override
-            public void onResponse(Call<VerifyPhoneModal> call, Response<VerifyPhoneModal> response) {
-                assert response.body() != null;
-                System.out.println("respo==="+new Gson().toJson(response));
-
-            }
-
-            @Override
-            public void onFailure(Call<VerifyPhoneModal> call, Throwable t) {
-                t.printStackTrace();
-                System.out.println("respo==="+t.getMessage());
-
-            }
-        });
-
+//        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST,)
     }
 }
